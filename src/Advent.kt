db9ba3010726f56d6632d07227114171c1995ac8
Maciej Procyk <macieekprocyk@gmail.com>
2024-12-13 00:39:27 +0100

    run only current day

--- src/Advent.kt
+++ src/Advent.kt
@@ -1,14 +1,22 @@
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
-import java.util.TreeMap
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.time.Duration
 import kotlin.time.measureTime
 import kotlin.time.measureTimedValue
 
 suspend fun main() {
-  val (solved, duration) = measureTimedValue { solveAdventDays() }
+  val now = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+  val (solved, duration) = measureTimedValue {
+    solveAdventDays(
+      solve = { day -> now.run { monthNumber != 12 || dayOfMonth > 25 || dayOfMonth == day } }
+    )
+  }
   solved.forEach { (day, duration) ->
     println("--- ${day::class.java.simpleName} ($duration)")
     day.lines.forEach(::println)
@@ -16,11 +24,16 @@ suspend fun main() {
   println("Total time: $duration")
 }
 
-suspend fun solveAdventDays(): List<Pair<AdventDay, Duration>> {
+suspend fun solveAdventDays(
+  solve: (day: Int) -> Boolean = { true },
+): List<Pair<AdventDay, Duration>> {
   val days = AdventDay.all
   val durations = ConcurrentHashMap<Int, Duration>()
   coroutineScope {
-    days.mapIndexed { idx, day -> async { durations[idx] = measureTime { day.solve() } } }.awaitAll()
+    days.mapIndexed { idx, day ->
+      if (solve(idx + 1)) async { durations[idx] = measureTime { day.solve() } }
+      else CompletableDeferred(value = Unit)
+    }.awaitAll()
   }
-  return days zip TreeMap(durations).values.toList()
+  return durations.toSortedMap().entries.map { days[it.key] to it.value }
 }
